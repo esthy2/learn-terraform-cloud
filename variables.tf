ad4f5ca9b7cf0d30dbed97630bdd360c4be66d81
esthy2 <esthyblack@yahoo.com>
2025-12-15 16:08:16 -0500

    Make assume_role_arn optional

--- variables.tf
+++ variables.tf
@@ -2,8 +2,11 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
-# variable "assume_role_arn" {
-# }
+variable "assume_role_arn" {
+  type    = string
+  default = ""
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
